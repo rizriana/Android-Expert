ac1b0d8f7d24434abed86cc4a5a52377253d7226
AnanditaRian19 <rizkianandita6@gmail.com>
2021-03-30 10:57:28 +0700

    Initial Commit

---
+++ 07-TourismApp CoroutinesFlow/app/src/main/java/com/dicoding/tourismapp/core/data/source/remote/network/ApiConfig.kt
@@ -0,0 +1,31 @@
+package com.dicoding.tourismapp.core.data.source.remote.network
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+
+/**
+ *@author Rizki Rian Anandita
+ * Create By rizki
+ */
+object ApiConfig {
+
+    private fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+            .readTimeout(120, TimeUnit.SECONDS)
+            .build()
+    }
+
+    fun provideApiService(): ApiService {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://tourism-api.dicoding.dev/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(provideOkHttpClient())
+            .build()
+        return retrofit.create(ApiService::class.java)
+    }
+
+}
