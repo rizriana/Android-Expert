c4c567b73d979f35c4d4ff012d04cbf68d39b28a
AnanditaRian19 <rizkianandita6@gmail.com>
2021-04-01 23:10:08 +0700

    Initial Commit

---
+++ 17-TourismApp LibraryModule DynamicFeature/core/src/main/java/com/dicoding/tourismapp/core/domain/repository/ITourismRepository.kt
@@ -0,0 +1,19 @@
+package com.dicoding.tourismapp.core.domain.repository
+
+import com.dicoding.tourismapp.core.data.Resource
+import com.dicoding.tourismapp.core.domain.model.Tourism
+import kotlinx.coroutines.flow.Flow
+
+/**
+ *@author Rizki Rian Anandita
+ * Create By rizki
+ */
+interface ITourismRepository {
+
+    fun getAllTourism(): Flow<Resource<List<Tourism>>>
+
+    fun getFavoriteTourism(): Flow<List<Tourism>>
+
+    fun setFavoriteTourism(tourism: Tourism, state: Boolean)
+
+}
